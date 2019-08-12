e2be3878cb29768da7ff65943f9792d5dd89497b
Matthew.Greco <matthew.greco85@gmail.com>
2019-08-12 09:35:14 -0500

    first test

--- code.js
+++ code.js
@@ -2,3 +2,11 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function sum(sum) {
+    return 1;
+}
+
+function sayHello(sayHello) {
+    return "Hello";
+}
--- tests.js
+++ tests.js
@@ -1,15 +1,35 @@
 // Unit tests for the helloWorld function
-describe('helloWorld', function() {
-    it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+describe('helloWorld', function () {
+    it('should be a defined function', function () {
+        expect(typeof helloWorld).toBe('function');
     });
-    it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+    it('should return a string when called', function () {
+        expect(typeof helloWorld()).toBe("string");
     });
-    it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
+    it('should return the string "Hello, World!" when executed', function () {
+        expect(helloWorld()).toBe("Hello, World!");
     });
-    it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+    it("should never return 'undefined' when called", function () {
+        expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sum', function () {
+    it('should be a defined function', function () {
+        expect(typeof sum).toBe('function');
+
+
+    });
+    it('should return the number 4 when passed the numbers 2 and 2', function () {
+    });
+
+});
+
+describe("sayHello", function () {
+    it('should be a defined function', function () {
+        expect(function () {
+
+        })
+    });
+});
+
